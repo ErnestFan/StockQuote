20d5f14554c70c0985f095e1dd5eab8047aabf1f
ErnestFan <ernestfan1019@gmail.com>
2019-11-13 10:52:45 -0800

    Initial Commit

---
+++ StockQuote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StockQuote
+//
+//  Created by Ernest Fan on 2019-11-13.
+//  Copyright © 2019 ErnestFan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
